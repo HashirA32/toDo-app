9568a9b9287c3e4f4213e46a98f36cf368a9278a
HashirA32 <hashirofficiala32@gmail.com>
2025-01-23 06:10:27 -0800

    Added post and delete methods

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,14 +1,27 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+
+let data = [{ item: "Orange" }, { item: "Mango" }, { item: "Banana" }, { item: "Apple" }];
+
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+let app = express();
+app.set("view engine", "ejs"); // Set EJS as the template engine
+
 module.exports = (app) => {
+  app.get("/todo", (req, res) => {
+    res.render("todo", { todo: data });
+  });
 
-    app.get("/todo", (req, res)=> {
-        res.render('todo');
-    });
-    
-    app.post("/todo", (req, res)=> {
+  app.post("/todo", urlencodedParser, (req, res) => {
+    data.push(req.body);
+    res.json(data); // Send back updated data as JSON
+  });
 
-    });
-     
-    app.delete("/todo", (req, res)=> {
-
-    });
-}
+  app.delete("/todo/:item", (req, res) => {
+    data = data.filter(()=> {
+      return todo.item.replace(/ /g, '-') !== req.params.item;
+    })
+    res.json(data);
+  });
+};
